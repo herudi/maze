b821d73b388f01fc8230a29e6f567559fb70cd20
herudi <herudi7@gmail.com>
2022-03-10 13:35:55 +0700

    add link

--- core/constant.ts
+++ core/constant.ts
@@ -1,3 +1,3 @@
-export const LINK = "https://github.com/herudi/maze/master";
+export const LINK = "https://raw.githubusercontent.com/herudi/maze/master";
 export const STORAGE_KEY_PAGE = "95d6aa06f620";
 export const STORAGE_KEY_API = "01a2929d18cf";
