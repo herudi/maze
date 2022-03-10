c84410ada9ca7ec9ae851ff44f56fa9f32d47d0c
herudi <herudi7@gmail.com>
2022-03-10 14:43:06 +0700

    router

--- cli/create.ts
+++ cli/create.ts
@@ -310,10 +310,147 @@ export const initApp = async (appCallback?: (app: NHttp<ReqEvent>) => any) => {
 };
 `);
   await Deno.writeTextFile(join(dir, '_core', 'hydrate.tsx'), `/** @jsx h */
-import { h } from "nano-jsx";
+import { h, hydrate } from "nano-jsx";
 import { pages, tt } from "./result/pages.ts";
 import RootApp from "./root_app.tsx";
-import ClassicRouter from "${link}/core/router.js";
+import { RequestEvent } from "types";
+
+type ReqEvent = RequestEvent & {
+  render: (elem: any, id?: string) => any;
+};
+
+type THandler = (
+  rev: ReqEvent,
+) => any;
+
+function wildcard(path: string, wild: boolean, match: any) {
+  const params = match.groups || {};
+  if (!wild) return params;
+  if (path.indexOf("*") !== -1) {
+    match.shift();
+    const wild = match.filter((el: any) => el !== void 0).filter((
+      el: string,
+    ) => el.startsWith("/")).join("").split("/");
+    wild.shift();
+    const ret = { ...params, wild: wild.filter((el: string) => el !== "") };
+    if (path === "*" || path.indexOf("/*") !== -1) return ret;
+    let wn = path.split("/").find((el: string) =>
+      el.startsWith(":") && el.endsWith("*")
+    );
+    if (!wn) return ret;
+    wn = wn.slice(1, -1);
+    ret[wn] = [ret[wn]].concat(ret.wild).filter((el) => el !== "");
+    delete ret.wild;
+    return ret;
+  }
+  return params;
+}
+
+function decURI(str: string) {
+  try {
+    return decodeURI(str);
+  } catch (_e) {
+    return str;
+  }
+}
+
+type TRouter = {
+  id?: string;
+  notFoundPage?: any;
+  errorPage?: any;
+};
+
+export default class ClassicRouter {
+  routes: { path: string; regex: RegExp; wild: boolean; fn: THandler }[] = [];
+  current: string | undefined;
+  id: string;
+  errorPage: any;
+  notFoundPage: any;
+  constructor(opts: TRouter = {}) {
+    this.id = opts.id || "root";
+    this.errorPage = opts.errorPage || (<div>Error: Something went wrong</div>);
+    this.notFoundPage = opts.notFoundPage || (<div>404: Page not found</div>);
+  }
+
+  add(path: string, fn: THandler) {
+    let wild = false;
+    const str = path
+      .replace(/\\/$/, "")
+      .replace(/:(\\w+)(\\?)?(\\.)?/g, "$2(?<$1>[^/]+)$2$3")
+      .replace(/(\\/?)\\*/g, (_, p) => {
+        wild = true;
+        return ${"`(${p}.*)?`"};
+      })
+      .replace(/\\.(?=[\\w(])/, "\\\\.");
+    const regex = new RegExp(${"`^${str}/*$`"});
+    this.routes.push({ path, fn, regex, wild });
+    return this;
+  }
+
+  find(pathname: string) {
+    let fn: any, params = {}, j = 0, el, arr = this.routes, len = arr.length;
+    pathname = decURI(pathname);
+    while (j < len) {
+      el = arr[j];
+      if (el.regex.test(pathname)) {
+        const match = el.regex.exec(pathname);
+        fn = el.fn;
+        params = wildcard(el.path, el.wild, match);
+        break;
+      }
+      j++;
+    }
+    return { fn, params };
+  }
+
+  handle() {
+    const { pathname, search, origin } = window.location;
+    if (this.current === pathname + search) return;
+    let { fn, params } = this.find(pathname);
+    this.current = pathname + search;
+    const _id = this.id;
+    const rev = {} as ReqEvent;
+    rev.pathname = pathname;
+    rev.url = this.current;
+    rev.path = pathname;
+    rev.isServer = false;
+    rev.getBaseUrl = () => origin;
+    rev.params = params;
+    rev.fetchApi = async (pathname, opts) => {
+      try {
+        const res = await fetch(origin + pathname, opts);
+        if (!res.ok) throw res;
+        const json = await res.json();
+        return { data: json, error: void 0 };
+      } catch (error) {
+        const json = await error.json();
+        json.message = decURI(json.message);
+        return { data: void 0, error: json };
+      }
+    };
+    rev.render = (elem, id) => {
+      hydrate(elem, document.getElementById(id || _id));
+    };
+    if (!fn) return rev.render(this.notFoundPage);
+    fn(rev);
+  }
+
+  resolve() {
+    const handle = () => this.handle();
+    handle();
+    window.addEventListener("pushstate", (e: any) => {
+      e.preventDefault();
+      handle();
+    });
+    window.addEventListener("replacestate", (e: any) => {
+      e.preventDefault();
+      handle();
+    });
+    window.addEventListener("popstate", () => {
+      handle();
+    });
+  }
+}
 
 async function lazy(url: string) {
   const mod = (await import(url + "?v=" + tt)).default;
