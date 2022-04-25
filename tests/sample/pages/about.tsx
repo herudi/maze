9a6830c02b5cf14ffd4f26b5aa95d6468efff8d1
herudi <herudi7@gmail.com>
2022-04-25 09:38:15 +0700

    fix test

--- tests/sample/pages/about.tsx
+++ tests/sample/pages/about.tsx
@@ -1,10 +1,12 @@
 /** @jsx h */
 import { Component, h } from "../deps_client.ts";
-import { InitProps, PageProps } from "../deps_client.ts";
+import { InitPage, PageProps } from "../deps_client.ts";
 
-@InitProps(async ({ fetchApi }) => {
-  const { data, error } = await fetchApi("/api/about");
-  return { data, error };
+@InitPage({
+  props: async ({ fetchApi }) => {
+    const { data, error } = await fetchApi("/api/about");
+    return { data, error };
+  },
 })
 export default class About extends Component<PageProps> {
   render() {
