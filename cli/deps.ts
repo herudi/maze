14277850b6df9245cddb6617325219fbc028dc61
herudi <herudi7@gmail.com>
2022-04-29 17:48:50 +0700

    esbuild

--- cli/deps.ts
+++ cli/deps.ts
@@ -4,7 +4,7 @@ export {
   toFileUrl,
 } from "https://deno.land/std@0.132.0/path/mod.ts";
 export { walk } from "https://deno.land/std@0.132.0/fs/walk.ts";
-export { default as esbuild } from "https://deno.land/x/esbuild@v0.14.25/mod.js";
+export * as esbuild from "https://deno.land/x/esbuild@v0.14.25/mod.js";
 export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.4.1/mod.ts";
 export function isExist(pathfile: string) {
   try {
